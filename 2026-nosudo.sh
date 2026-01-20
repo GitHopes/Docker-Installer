2522004f0c39b0157442b5ad02076603c5e1b9ee
Franco <franco323010@gmail.com>
2026-01-20 10:32:15 +0000

    Agregar script para eliminar contenedores y configurar Docker en Ubuntu

---
+++ 2026-nosudo.sh
@@ -0,0 +1,35 @@
+echo "Este script eliminara todos los contenedores que hayan previamente"
+
+apt-get remove --purge docker docker-engine docker.io containerd runc docker-compose
+rm -rf /var/lib/docker
+rm -rf /var/lib/containerd
+
+# Actualizar paquetes APT
+apt-get update
+
+# Instalar los paquetes necesarios para configurar el repositorio oficial de Docker
+apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release -y
+
+# Agregar la clave GPG oficial de Docker
+kdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+# Añadir el repositorio de Docker a APT
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Activar el repositorio APT de Docker
+apt-get update
+
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+docker run hello-world
+
+usermod -aG docker $USER
+
+echo "Reincia el ordenador y ejecuta 'docker version' para ver la version"
